18a1357083550716163928977f3895a845dacf8a
chin0 <pypi1103@gmail.com>
2017-03-13 12:00:45 +0900

    오일러 프로젝트 12번 문제 추가

---
+++ euler/12.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+
+typedef unsigned long long ull;
+
+using namespace std;
+
+ull get_tri_number(ull n)
+{
+    return (n * n + n) / 2;
+}
+
+int count_prime_number(ull tri_number)
+{
+    int result = 1;
+
+    if(tri_number == 1)
+        return -1;
+    
+    vector<int> prime_numbers;
+    for(int i = 2; tri_number > 1; i++)
+        while(tri_number % i == 0)
+        {
+            tri_number /= i;
+            prime_numbers.push_back(i);
+        }
+    
+    int a = prime_numbers[0];
+    int count = 1;
+    for(int i = 1; i < prime_numbers.size(); i++){
+        if(a == prime_numbers[i])
+            count++;
+        else{
+            result *= count + 1;
+            a = prime_numbers[i];
+            count = 1;
+        }
+    }
+    result *= count + 1;
+            
+    
+    return result;        
+}
+
+int main(void)
+{
+    ull n = 2;
+    for(;;)
+    {
+        ull temp = get_tri_number(n);
+        int prime_count = count_prime_number(temp);
+        if(prime_count >= 500)
+        {
+            cout << temp << endl;
+            return 0;
+        }
+        n++;
+    }
+}
+
+
+    
+
